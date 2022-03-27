7ced41e7e2daff321059f07fb001da6c857f9a1e
Nick Fairchild <nick.fairchild@gmail.com>
2022-03-27 21:19:11 +0100

    tidyup

--- functions.php
+++ functions.php
@@ -2,8 +2,9 @@
 
 if (file_exists(__DIR__.'/vendor/autoload.php')) {
     require_once __DIR__.'/vendor/autoload.php';
+    require_once __DIR__.'/inc/helpers.php';
 }
-require_once __DIR__.'/inc/helpers.php';
 require_once __DIR__.'/inc/setup.php';
 require_once __DIR__.'/inc/options-page.php';
 require_once __DIR__.'/inc/filters.php';
+require_once __DIR__.'/inc/tidyup.php';
--- inc/setup.php
+++ inc/setup.php
@@ -75,25 +75,3 @@ add_action('init', function () {
 add_action('widgets_init', function () {
     //
 });
-
-add_action('wp_resource_hints', function (array $urls): array {
-    for ($i = 0, $iMax = count($urls); $i < $iMax; $i++) {
-        // Removes <link rel="dns-prefetch" href="//s.w.org"> which is
-        // preloaded for emojis...
-        if (strpos($urls[$i], 'emoji') !== false) {
-            unset($urls[$i]);
-            break;
-        }
-    }
-
-    return $urls;
-});
-
-add_action('footer', function () {
-    wp_deregister_script('wp-embed');
-});
-
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'wp_shortlink_wp_head');
