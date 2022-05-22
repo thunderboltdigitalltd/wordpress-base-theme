cb785c9559f17b79ec2a0916eb0c54c04ba8a8ae
Nick Fairchild <nick.fairchild@gmail.com>
2022-05-22 10:57:36 +0100

    bundle plugins

--- functions.php
+++ functions.php
@@ -8,3 +8,5 @@ require_once __DIR__.'/inc/setup.php';
 require_once __DIR__.'/inc/options-page.php';
 require_once __DIR__.'/inc/filters.php';
 require_once __DIR__.'/inc/tidyup.php';
+require_once __DIR__.'/inc/class-tgm-plugin-activation.php';
+require_once __DIR__.'/inc/plugins.php';
